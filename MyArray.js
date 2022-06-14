6ea93857aba81446031a541482935e5c670a7150
Maksim <maks.s375@gmail.com>
2022-06-14 14:05:21 +0300

    add while example

---
+++ MyArray.js
@@ -0,0 +1,15 @@
+// this.forEach = function (callback) {
+//   for (let i = 0; i < this.length; i++) {
+//     callback(this[i], i, this);
+//   }
+// };
+
+
+// this.every = function(callback){
+//   for (let i = 0; i < this.length; i++) {
+//     if(callback(this[i],i,this) === false ){
+//       return false;
+//     }
+//   }
+//   return true;
+// }
